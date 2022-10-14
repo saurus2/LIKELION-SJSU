707fc1d0016dc54b97fb83e1f2e9c3db2999556a
TaehunLeee5 <TaehunLeee5@gmail.com>
2022-10-13 17:46:40 -0700

    2nd attempt

--- Team3_Project1/todo_list/script.js
+++ Team3_Project1/todo_list/script.js
@@ -1,1 +1,16 @@
 //script
+var i = 0;
+function add() {
+    var input = document.getElementById('td1').value;
+    if (!input) {
+        alert('내용을 입력해 주세요')
+    } else {
+        var node = document.createElement("SPAN");
+        var textnode = document.createTextNode(input);
+        node.id = node + i;
+        node.appendChild(textnode);
+        i++;
+        document.getElementById("list").appendChild(node)
+        document.getElementById('td1').value = "";
+    }
+}
