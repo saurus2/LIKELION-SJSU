d28e09ce2a935f3d85a7beb4099265dff4a0fa4b
TaehunLeee5 <TaehunLeee5@gmail.com>
2022-10-15 14:51:49 -0700

    Team3 update

--- Team3_Project1/todo_list/script.js
+++ Team3_Project1/todo_list/script.js
@@ -2,15 +2,40 @@
 var i = 0;
 function add() {
     var input = document.getElementById('td1').value;
-    if (!input) {
-        alert('내용을 입력해 주세요')
+    var inputdate = document.getElementById('date').value;
+    
+    if (!input||!inputdate) {
+        alert('내용&날짜를 입력해 주세요')
     } else {
         var node = document.createElement("SPAN");
-        var textnode = document.createTextNode(input);
-        node.id = node + i;
+        var checkbox = document.createElement("INPUT");
+        checkbox.setAttribute("type", "checkbox");
+
+
+        var textnode = document.createTextNode(input + ' - ' + document.getElementById('date').value);
+        node.id = 'node' + i;
+        checkbox.id = 'checkbox' + i;
         node.appendChild(textnode);
-        i++;
+        document.getElementById("list").appendChild(checkbox)
         document.getElementById("list").appendChild(node)
-        document.getElementById('td1').value = "";
+        document.getElementById('td1').value = ""; 
+        document.getElementById('date').value ="";
+        removeButton(i);
+        i++;
     }
+}
+
+function removeButton(n){
+    var remove_btn = document.createElement("Button");
+    remove_btn.innerHTML = "-";
+    remove_btn.id = 'del' + n;
+    remove_btn.className = "minus";
+    remove_btn.onclick = function() {remove(n)};
+    document.getElementById("list").appendChild(remove_btn);
+}
+
+function remove(n) {
+    document.getElementById('node'+n).style.display='none'; 
+    document.getElementById('del'+n).style.display='none';
+    document.getElementById('checkbox'+n).style.display='none';
 }
