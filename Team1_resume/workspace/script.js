fc752f06d1fd051dc63cbed76269a0c07f686bb4
Kelly1004 <dearkelly4129@gmail.com>
2022-10-16 20:02:45 -0700

    resume edit

--- Team1_resume/workspace/script.js
+++ Team1_resume/workspace/script.js
@@ -11,8 +11,11 @@ function addNewWeField(){
          //newNode.setAttribute("cols", 10);
     
     let we44 = document.getElementById("we33");
-     weAddButton.prepend(newNode);
-      //we44.append(newNode);
+    we11.insertBefore(newNode, we33);
+
+    // weAddButton.prepend(newNode);
+   
+    //we44.append(newNode);
      //newNode.append(we44);
   }
    
@@ -24,17 +27,31 @@ function addNewWeField(){
      newNode.setAttribute("rows", 3);
      newNode.setAttribute("placeholder", "Enter Here");
    
-     let we66 = document.getElementById("we77");
-     afieldAddButton.prepend(newNode);
-   
+     let we88 = document.getElementById("we77");
+    // afieldAddButton.prepend(newNode);
+     we55.insertBefore(newNode, we77);
+
    
   }
    
-
   function sound() {
   var audio = new Audio("sounds/Claps.wav");
   document.onclick = function() {
     audio.play();
   }
 }
+
+//resume generate function
+//submit --> onsubmit --> submit.onsubmit(resume.page)
+
+  function generate(){
+    document.getElementById('click').innerHTML
+     = document.getElementById('nameField').value ;
+  }
+
+
+
+
+
+
   
