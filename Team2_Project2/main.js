b3486ece9b8f5645a1170c536cf5e0c7b6ec3b9b
haminse <haminse@naver.com>
2022-11-19 14:14:54 -0800

    minse

---
+++ Team2_Project2/main.js
@@ -0,0 +1,25 @@
+        // when click food btn
+        $('#food_btn').click(function(){
+            $('.menu').css('display', 'none');
+            $('.food').css('display', 'block');
+        });
+
+        // when click date btn
+        $('#date_btn').click(function(){
+            $('.menu').css('display', 'none');
+            $('.date').css('display', 'block');
+        });
+
+        // when click drama btn
+        $('#drama_btn').click(function(){
+            $('.menu').css('display', 'none');
+            $('.drama').css('display', 'block');
+        });
+
+        // when click home button
+        $('.home').click(function(){
+            $('.menu').css('display', 'block');
+            $('.sub').css('display', 'none');
+        });
+
+        
