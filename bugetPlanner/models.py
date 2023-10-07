c63f2fe792c4a7f78aa4729908e7bf0dfae24b5f
ken7184 <kenkim7184@gmail.com>
2023-10-07 14:05:26 -0700

    Complete DB

---
+++ bugetPlanner/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    Income = models.IntegerField(verbose_name= 'Income')
+    Savings = models.IntegerField(verbose_name= 'Savings', default=0, blank = True)
+    Remaining = models.IntegerField(verbose_name= 'Remaining Income', default=0)
+
+
+class Login(models.Model):
+    UserId = models.ForeignKey(to = "Users", on_delete = models.CASCADE)
+    LoginName = models.TextField(verbose_name="Name")
+    LoginPassword = models.TextField(verbose_name="Password")
+
+class Expenditures(models.Model):
+
+    rent = models.IntegerField(verbose_name= 'Rent', default=0, blank = True)
+    leisure = models.IntegerField(verbose_name= 'Leisure', default=0, blank = True)
+    grocery = models.IntegerField(verbose_name= 'Grocery', default=0, blank = True)
+    others = models.IntegerField(verbose_name= 'Others', default=0, blank = True)
+    month = models.IntegerField(verbose_name= 'Month', default=0, blank = True)
+    year = models.IntegerField(verbose_name= 'Year', default=0, blank = True)
+    UserId = models.ForeignKey(to = "Users", on_delete = models.CASCADE)
